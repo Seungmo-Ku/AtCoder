2a67105bf1ac06784e0738832963345daf0e501b
Seungmo Ku <88317380+Seungmo-Ku@users.noreply.github.com>
2025-01-28 19:41:57 +0900

    Update 250123_C_AvoidKnightAttack.cpp
    TLE error

--- AtCoderBeginnerContest377/250123_C_AvoidKnightAttack.cpp
+++ AtCoderBeginnerContest377/250123_C_AvoidKnightAttack.cpp
@@ -1,13 +1,19 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 
 struct Point {
     int x, y;
-    bool operator==(const Point &p) {
+    bool operator==(const Point &p) const {
         return x == p.x && y == p.y;
+    }
+    bool operator<(const Point &p) const {
+        if(x == p.x) return y < p.y;
+        else return x < p.x;
+        //return x < p.x || (x == p.x && y < p.y);
     }
 };
 
@@ -27,37 +33,40 @@ int main(void) {
         cin >> coord.x >> coord.y;
         coord.x --;
         coord.y --;
-        points.push_back(coord);
+        if(checkValidSquare(coord.x, coord.y, N, points))points.push_back(coord);
         //(x, y+1), (x, y-3), (x+1, y), (x+1, y-2), (x-2, y+1), (x-2, y-3), (x-3, y), (x-3, y-2)
-        coord.x ++; coord.y ++;
-        if(checkValidSquare(coord.x, coord.y + 1, N, points)) {
-            Point coord2 = {coord.x, coord.y + 1};
+        //coord.x ++; coord.y ++;
+        if(checkValidSquare(coord.x + 1, coord.y + 2, N, points)) {
+            Point coord2 = {coord.x + 1, coord.y + 2};
             points.push_back(coord2);
-        }if(checkValidSquare(coord.x + 1, coord.y, N, points)) {
-            Point coord2 = {coord.x + 1, coord.y};
+        }if(checkValidSquare(coord.x + 2, coord.y + 1, N, points)) {
+            Point coord2 = {coord.x + 2, coord.y + 1};
+            points.push_back(coord2);
+        }if(checkValidSquare(coord.x + 2, coord.y - 1, N, points)) {
+            Point coord2 = {coord.x + 2, coord.y - 1};
             points.push_back(coord2);
         }if(checkValidSquare(coord.x + 1, coord.y - 2, N, points)) {
             Point coord2 = {coord.x + 1, coord.y - 2};
             points.push_back(coord2);
-        }if(checkValidSquare(coord.x, coord.y - 3, N, points)) {
-            Point coord2 = {coord.x, coord.y - 3};
+        }if(checkValidSquare(coord.x - 1, coord.y - 2, N, points)) {
+            Point coord2 = {coord.x - 1, coord.y - 2};
             points.push_back(coord2);
-        }if(checkValidSquare(coord.x - 2, coord.y - 3, N, points)) {
-            Point coord2 = {coord.x - 2, coord.y - 3};
-            points.push_back(coord2);
-        }if(checkValidSquare(coord.x - 3, coord.y - 2, N, points)) {
-            Point coord2 = {coord.x - 3, coord.y - 2};
-            points.push_back(coord2);
-        }if(checkValidSquare(coord.x - 3, coord.y, N, points)) {
-            Point coord2 = {coord.x - 3, coord.y};
+        }if(checkValidSquare(coord.x - 2, coord.y - 1, N, points)) {
+            Point coord2 = {coord.x - 2, coord.y - 1};
             points.push_back(coord2);
         }if(checkValidSquare(coord.x - 2, coord.y + 1, N, points)) {
             Point coord2 = {coord.x - 2, coord.y + 1};
             points.push_back(coord2);
+        }if(checkValidSquare(coord.x - 1, coord.y + 2, N, points)) {
+            Point coord2 = {coord.x - 1, coord.y + 2};
+            points.push_back(coord2);
         }
         
     }
-    cout << N*N - points.size() << endl;
+
+    sort(points.begin(), points.end());
+    points.erase(unique(points.begin(), points.end()), points.end());
+    cout << (long)N*N - points.size() << endl;
     
    
 
@@ -65,9 +74,9 @@ int main(void) {
 }
 
 bool checkValidSquare(int x, int y, int N, vector<Point> points) {
-    Point coord = {x, y};
-    auto it = find(points.begin(), points.end(), coord);
-    if(it != points.end()) return false;
+    //Point coord = {x, y};
+    //auto it = find(points.begin(), points.end(), coord);
+    //if(it != points.end()) return false;
 
     return x >= 0 && x < N && y >= 0 && y < N;
 }
